6feef69e7b305351c07365de2595b5dd65efbfee
elton lima <eltonathayde2@gmail.com>
2022-03-12 11:04:45 -0300

    ativando a função de deletar categoria,

--- categories/categoreisCrontroller.js
+++ categories/categoreisCrontroller.js
@@ -5,7 +5,8 @@ const router = express.Router();
 // carregando model de categoria 
 const Category = require("./Category");
 // importando a biblioteca slugify, para transforma uma string em slug
-const slugify = require('slugify')
+const slugify = require('slugify');
+const { redirect } = require('express/lib/response');
 
 // rota para adiconar uma nova categoria, acesso so com painel de adm
 router.get("/admin/categories/new",(req,res)=>{
@@ -21,7 +22,7 @@ router.post("/categories/save",(req,res)=>{
             title: title,
             slug: slugify(title)
         }).then(()=>{
-            res.redirect("/"); 
+            res.redirect("/admin/categories"); 
         })
 
     }else{
@@ -33,10 +34,57 @@ router.get("/admin/categories",(req,res)=>{
     // importando o module que renderiza essa tela 
     Category.findAll().then(categories=>{
         res.render("admin/categories/index",{categories:categories});
-    })
-
-
-   
+    });
 });
 
+// criando uma rota para deletar categorias 
+router.post("/categories/delete",(req,res)=>{
+    var id = req.body.id;
+    // verificando se o id e difente de  nulo 
+    if(id != undefined){
+
+        if(!isNaN(id)){
+
+            Category.destroy({
+                    where: {
+                        id:id
+                    }
+            }).then(()=>{
+                res.redirect("/admin/categories");
+            });
+
+
+        }else{ // se o id não for um numero é redirecionado para pagina de listagem de   categorias
+            res.redirect("/admin/categories");
+        }
+
+    }else{ // se o ider for nulo, e redirecionado para pagina  de listagem de categorias 
+        res.redirect("/admin/categories");
+    }
+});
+// rota de edição de categorias
+ router.get("/admin/categories/edit/:id",(req,res)=>{
+
+    var id= req.params.id;
+      //  metedo especifico para pesquisar uma categoria pelo id dela 
+
+    // Verificando se o id não é um numero 
+    if(isNaN(id)){
+        res.redirect("/admin/categorties"); 
+    }
+
+
+    Category.findByPk(id).then(categoria =>{
+        if(categoria != undefined){
+
+            res.render("admin/categories/edit",{categoria:categoria});
+
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro=>{
+        res.redirect("/admin/categories");
+    })
+
+ });
 module.exports = router; 
